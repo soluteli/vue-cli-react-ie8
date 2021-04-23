0576610d116fa25f28fabacc54908419076a24db
soluteli <soluteli@163.com>
2021-04-23 16:51:13 +0800

    fix: 添加 Button

--- components/antd/index.example.jsx
+++ components/antd/index.example.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Tag } from "antd";
+import { Tag, Button } from "antd";
 
 export default {
   title: 'antd-demo'
 }
 
 export function TagBasic () {
-  return <Tag>123</Tag>
+  return <Tag color="blue">123</Tag>
 }
 export function TagRemove () {
   return <Tag closable>123</Tag>
+}
+export function Button1 () {
+  return <Button type="primary">1234</Button>
 }
